47652444614b2cd937d6ebcd9395209eb761ca05
Terence1991 <cplterencehawes@gmail.com>
2019-01-02 21:01:38 +0000

    aprove button added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from './CommentDetails.js'
+import ApprovalCard from './ApprovalCard.js'
 import faker from 'faker';
 
 const App = () => {
   return (
  <div className='ui container comments'>
+    <ApprovalCard/>
    <CommentDetail author='sam' timeAgo="Today at 4:50 pm" content="I love code" avatar={faker.image.avatar()}/>
  </div>
 );
